63dce018587892eb145f402f88ec71fa8a32ec82
Don Bottstein <don@bottstein.com>
2018-05-24 17:15:16 -0600

    remove zplug, didnt work

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,7 @@
 BASE_DIR=$(dirname $0)/..
 
-test -f "$HOME/.zshenv" || cp $BASE_DIR/zsh/dotfiles/zshenv $HOME/.zshenv
-test -f "$HOME/.zprofile" || cp $BASE_DIR/zsh/dotfiles/zshenv $HOME/.zprofile
-test -f "$HOME/.zlogin" || cp $BASE_DIR/zsh/dotfiles/zshenv $HOME/.zlogin
-test -f "$HOME/.zshrc" || cp $BASE_DIR/zsh/dotfiles/zshenv $HOME/.zshrc
+test -f "$ZDOTDIR/.zshenv" || cp $BASE_DIR/zsh/dotfiles/zshenv $ZDOTDIR/.zshenv
+test -f "$ZDOTDIR/.zprofile" || cp $BASE_DIR/zsh/dotfiles/zshenv $ZDOTDIR/.zprofile
+test -f "$ZDOTDIR/.zlogin" || cp $BASE_DIR/zsh/dotfiles/zshenv $ZDOTDIR/.zlogin
+test -f "$ZDOTDIR/.zshrc" || cp $BASE_DIR/zsh/dotfiles/zshenv $ZDOTDIR/.zshrc
+test -f "$ZDOTDIR/.zplugrc" || cp $BASE_DIR/zsh/dotfiles/zplugrc $ZDOTDIR/.zplugrc
