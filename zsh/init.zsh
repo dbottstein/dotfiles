cbb0e12bfe828ace71cd7e2036fd20076db32f42
Don Bottstein <don@bottstein.com>
2018-06-16 23:44:03 -0600

    comment some code

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -23,11 +23,11 @@ zstyle ':prezto:module:prompt:*' colors prompt yellow user 244
 
 zstyle -t ':prezto:module:prompt:*' symbols prompt || zstyle ':prezto:module:prompt:*' symbols prompt "$prompt_symbol"
 
-zstyle ':vcs_info:*:prompt:*:oppa_lana_style' formats \
-            "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %~ %F{57}%k"$'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
-            "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %25<...<%~%<< %F{57}%K{white}" \
-            $'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
-            "%(?..%F{$red}%? ↵%f)"
+#zstyle ':prezto:module:prompt:oppa_lana' formats \
+#            "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %~ %F{57}%k"$'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
+#            "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %25<...<%~%<< %F{57}%K{white}" \
+#            $'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
+#            "%(?..%F{$red}%? ↵%f)"
 
 source $zdotdir/.dotfiles/zsh/dot/zplugrc.prezto
 
