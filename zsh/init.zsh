ada3617f181e7f76fd7c16c822b6760188ca20a3
Don Bottstein <don@bottstein.com>
2018-06-17 21:46:12 -0600

    fixes for oppa-lana-style

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -13,21 +13,22 @@ if [ -z "$prompt_symbol" ]; then
     esac
 fi
 
-#echo -e "\033]6;1;bg;red;brightness;18\a"
-#echo -e "\033]6;1;bg;green;brightness;26\a"
-#echo -e "\033]6;1;bg;blue;brightness;33\a"
+# \033[38;2;<r>;<g>;<b>m     #Select RGB foreground color
+# \033[48;2;<r>;<g>;<b>m     #Select RGB background color
+# echo "\033[38;2;255;82;197;48;2;155;106;0m"
+# background=$'\e[48;2;0;85;85m'
 
-zstyle ':vcs_info:*' enable git
-zstyle ':prezto:module:prompt:*' colors prompt yellow user 244
-#zstyle ':vcs_info:*:prompt:*:*' symbols return ""
+zstyle ':prezto:module:prompt:qoup'            colors prompt yellow user 244
+zstyle ':prezto:module:prompt:oppa-lana-style' colors prompt_foreground yellow user_foreground 244
+#zstyle ':prezto:module:prompt:*' symbols return ""
 
-zstyle -t ':prezto:module:prompt:*' symbols prompt || zstyle ':prezto:module:prompt:*' symbols prompt "$prompt_symbol"
+zstyle -t ':prezto:module:prompt:*' symbols prompt || zstyle ':prezto:module:prompt:*' symbols prompt "${prompt_symbol}"
 
-#zstyle ':prezto:module:prompt:oppa_lana' formats \
-#            "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %~ %F{57}%k"$'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
-#            "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %25<...<%~%<< %F{57}%K{white}" \
-#            $'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
-#            "%(?..%F{$red}%? ↵%f)"
+zstyle ':prezto:module:prompt:oppa-lana-style' 'prompt-formats' \
+            'no-git'  "%F{white}%K{blue} %~ %F{blue}%k"
+#             suffix "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %~ %F{57}%k"$'\n'" %F{yellow}%k${prompt_symbol} %f%k%b" \
+#             prefix "%F{244}%K{239} %n@%m %F{239}%K{57}%F{white} %25<...<%~%<< %F{57}%K{white}" \
+#             rprompt "%(?..%F{$red}%? ↵%f)" \
 
 source $zdotdir/.dotfiles/zsh/dot/zplugrc.prezto
 
