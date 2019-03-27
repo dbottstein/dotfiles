fd88308bab010e03991dd126392b73ea7bc561ae
Don Bottstein <donald.bottstein@jeppesen.com>
2019-03-27 16:37:44 -0600

    fix install script

--- bin/install.sh
+++ bin/install.sh
@@ -5,9 +5,9 @@ zdotdir=${ZDOTDIR:-$HOME}
 
 test -f "$zdotdir/.zshenv" || cp -v $BASE_DIR/zsh/dot/zshenv $zdotdir/.zshenv
 test -f "$zdotdir/.zprofile" || cp -v $BASE_DIR/zsh/dot/zprofile $zdotdir/.zprofile
-test -f "$zdotdir/.zlogin" || cp -v $BASE_DIR/zsh/dot/zshenv $zdotdir/.zlogin
-test -f "$zdotdir/.zshrc" || cp -v $BASE_DIR/zsh/dot/zshenv $zdotdir/.zshrc
-test -f "$zdotdir/.zplugrc" || cp -v $BASE_DIR/zsh/dot/zplugrc $zdotdir/.zplugrc
+test -f "$zdotdir/.zlogin" || cp -v $BASE_DIR/zsh/dot/zlogin $zdotdir/.zlogin
+test -f "$zdotdir/.zshrc" || cp -v $BASE_DIR/zsh/dot/zshrc $zdotdir/.zshrc
+test -f "$zdotdir/.zplugrc" || cp -v $BASE_DIR/zsh/dot/zplugrc.prezto $zdotdir/.zplugrc
 
 test -f "$zdotdir/.bash_profile" || cp -v $BASE_DIR/bash/dot/bash_profile $zdotdir/.bash_profile
 test -f "$zdotdir/.bashrc" || cp -v $BASE_DIR/bash/dot/bashrc $zdotdir/.bashrc
