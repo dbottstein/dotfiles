6044e0862c9c66cf1856388a064b2259e1dec5ab
Don Bottstein <don@bottstein.com>
2022-03-27 14:12:26 -0600

    disable iterm2 integration if not in iterm2

--- fish/plugins/iterm2/conf.d/iterm2.fish
+++ fish/plugins/iterm2/conf.d/iterm2.fish
@@ -1,4 +1,6 @@
-set --local plugin_dir (status dirname)
+if test "$ITERM_PROGRAM" = "iTerm.app"
+    set --local plugin_dir (status dirname)
 
-test -f "$plugin_dir/iterm2_shell_integration.fish"
-    and source "$plugin_dir/iterm2_shell_integration.fish"
+    test -f "$plugin_dir/../iterm2_shell_integration.fish"
+        and source "$plugin_dir/../iterm2_shell_integration.fish"
+end
