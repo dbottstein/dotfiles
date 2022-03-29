c70509fe134c529491e0d941d5bcc6c1638f1b50
Don Bottstein <don@bottstein.com>
2022-03-29 09:42:33 -0600

    zsh tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,3 @@
 if status is-interactive
-    set -gx HOMEBREW_GITHUB_API_TOKEN (head -1 $XDG_STATE_HOME/brew/github_api_token)
+    set -gx HOMEBREW_GITHUB_API_TOKEN (head -n 1 $XDG_STATE_HOME/brew/github_api_token)
 end
