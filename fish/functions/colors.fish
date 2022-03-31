e4248db539e976569ed6e3574908ba37fcbc1c3c
Don Bottstein <donald.bottstein@boeing.com>
2022-03-31 23:46:27 +0000

    fish: add cygwin plugin; fix colors.fish

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -1,8 +1,18 @@
 function colors -d "Show 256 TERM colors"
-  set -l X (tput op)
-  set -l Y (printf %$((COLUMNS-6))s)
-  for i in (set 0 256)
-  	set -l o 00$i;
-  	echo -e ${o:${#o}-3:3} $(tput setaf $i;tput setab $i)${Y// /=}$X;
-  end
+    for c in (seq 0 15)
+        printf '\e[38;5;%dm%03d\e[38;5;240m\u2502' $c $c
+    end
+    printf '\e[0m\n'
+
+    for i in (seq 0 5)
+        for c in (seq (math "($i * 36) + 16") (math "($i * 36) + 51"))
+            printf '\e[38;5;%dm%03d\e[38;5;240m\u2502' $c $c
+        end
+        printf '\e[0m\n'
+    end
+
+    for c in (seq 232 255)
+        printf '\e[38;5;%dm%03d\e[38;5;240m\u2502' $c $c
+    end
+    printf '\e[0m\n'
 end
