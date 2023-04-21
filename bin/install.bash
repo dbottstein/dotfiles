973250054b78107a7c8a8b40ea90804aa928aa04
Don Bottstein <don@ip-10-33-92-146.us-west-2.compute.internal>
2023-04-21 21:16:00 +0000

    more fixes

--- bin/install.bash
+++ bin/install.bash
@@ -4,4 +4,4 @@ test -f ~/.bash_profile && mv -nv ~/.bash_profile ~/.bash_profile_orig
 test -f ~/.bashrc && mv -nv ~/.bashrc ~/.bashrc_orig
 
 test -L ~/.bash_profile || ln -sv ${XDG_CONFIG_HOME:-$HOME/.config}/bash/dot/bash_profile ~/.bash_profile
-test -L ~/.bashrc || ln -sv ${XDG_CONFIG_HOME:-$HOME/.config}/bash/dot/bashrrc ~/.bashrc
+test -L ~/.bashrc || ln -sv ${XDG_CONFIG_HOME:-$HOME/.config}/bash/dot/bashrc ~/.bashrc
--- bin/install.sh
+++ bin/install.sh
@@ -7,11 +7,17 @@ test -d ${XDG_CACHE_HOME:-$HOME/.cache} || mkdir -p ${XDG_CACHE_HOME:-$HOME/.cac
 test -d ${XDG_DATA_HOME:-$HOME/.local/share} || mkdir -p ${XDG_DATA_HOME:-$HOME/.local/share}
 test -d ${XDG_STATE_HOME:-$HOME/.local/state} || mkdir -p ${XDG_STATE_HOME:-$HOME/.local/state}
 
-test -L ${XDG_CONFIG_HOME:-$HOME/.config}/bash || ln -sv $base_path/bash $XDG_CONFIG_HOME/
-test -L ${XDG_CONFIG_HOME:-$HOME/.config}/zsh || ln -sv $base_path/zsh $XDG_CONFIG_HOME/
-test -L ${XDG_CONFIG_HOME:-$HOME/.config}/fish || ln -sv $base_path/fish $XDG_CONFIG_HOME/
-test -L ${XDG_CONFIG_HOME:-$HOME/.config}/git || ln -sv $base_path/git $XDG_CONFIG_HOME/
-test -L ${XDG_CONFIG_HOME:-$HOME/.config}/vim || ln -sv $base_path/vim $XDG_CONFIG_HOME/
+test -d ${XDG_CONFIG_HOME:-$HOME/.config}/bash && mv ${XDG_CONFIG_HOME:-$HOME/.config}/bash ${XDG_CONFIG_HOME:-$HOME/.config}/bash_orig
+test -d ${XDG_CONFIG_HOME:-$HOME/.config}/zsh  && mv ${XDG_CONFIG_HOME:-$HOME/.config}/zsh  ${XDG_CONFIG_HOME:-$HOME/.config}/zsh_orig
+test -d ${XDG_CONFIG_HOME:-$HOME/.config}/fish && mv ${XDG_CONFIG_HOME:-$HOME/.config}/fish ${XDG_CONFIG_HOME:-$HOME/.config}/fish_orig
+test -d ${XDG_CONFIG_HOME:-$HOME/.config}/git  && mv ${XDG_CONFIG_HOME:-$HOME/.config}/git ${XDG_CONFIG_HOME:-$HOME/.config}/git_orig
+test -d ${XDG_CONFIG_HOME:-$HOME/.config}/vim  && mv ${XDG_CONFIG_HOME:-$HOME/.config}/vim ${XDG_CONFIG_HOME:-$HOME/.config}/vim_orig
+
+test -L ${XDG_CONFIG_HOME:-$HOME/.config}/bash || ln -sv $base_path/bash ${XDG_CONFIG_HOME:-$HOME/.config}/
+test -L ${XDG_CONFIG_HOME:-$HOME/.config}/zsh  || ln -sv $base_path/zsh ${XDG_CONFIG_HOME:-$HOME/.config}/
+test -L ${XDG_CONFIG_HOME:-$HOME/.config}/fish || ln -sv $base_path/fish ${XDG_CONFIG_HOME:-$HOME/.config}/
+test -L ${XDG_CONFIG_HOME:-$HOME/.config}/git  || ln -sv $base_path/git ${XDG_CONFIG_HOME:-$HOME/.config}/
+test -L ${XDG_CONFIG_HOME:-$HOME/.config}/vim  || ln -sv $base_path/vim ${XDG_CONFIG_HOME:-$HOME/.config}/
 
 mkdir -p ~/.local/bin ~/.local/lib
 
