28c8b5cf1fd735cc6078e74806299f8da560a2ff
Don Bottstein <don@bottstein.com>
2024-08-04 15:48:11 -0600

    init tweaks

--- bin/init.fish
+++ bin/init.fish
@@ -5,10 +5,10 @@ set -qU XDG_CACHE_HOME;  or set --universal --export XDG_CACHE_HOME $HOME/.cache
 set -qU XDG_CONFIG_DIRS; or set --universal --export --path XDG_CONFIG_DIRS /etc/xdg
 set -q XDG_DATA_DIRS;    or set --universal --export --path XDG_DATA_DIRS /usr/local/share /usr/share
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
 fish_add_path ~/.local/bin /usr/local/bin
 set --universal --export fisher_path $XDG_DATA_HOME/fisher
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 fisher install oh-my-fish/plugin-foreign-env
 fisher install oh-my-fish/theme-bobthefish
