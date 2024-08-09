4e8642e13dbe4221e1c695c96300fff569abb3cb
Don Bottstein <don@bottstein.com>
2024-08-09 12:39:16 -0600

    update colored_man_pages plugin

--- fish/plugins/colored_man_pages/functions/cless.fish
+++ fish/plugins/colored_man_pages/functions/cless.fish
@@ -1,8 +1,8 @@
 function cless -d "Configure less to colorize styled text using environment variables before executing a command that will use less"
-    set -q colored_man_blink; and set -l blink (set_color $colored_man_blink); or set -l blink (set_color -o red)
-    set -q colored_man_bold; and set -l bold (set_color $colored_man_bold); or set -l bold (set_color -o 0280A5)
-    set -q colored_man_standout; and set -l standout (set_color $colored_man_standout); or set -l standout (set_color F0CB02)
-    set -q colored_man_underline; and set -l underline (set_color $colored_man_underline); or set -l underline (set_color -u 5BA502)
+    set -q colored_man_blink; and set -l blink (set_color -o $colored_man_blink); or set -l blink (set_color -o red)
+    set -q colored_man_bold; and set -l bold (set_color -o $colored_man_bold); or set -l bold (set_color -o 0280A5)  # 5fafd7
+    set -q colored_man_standout; and set -l standout (set_color -r $colored_man_standout); or set -l standout (set_color -r F0CB02) # 949494
+    set -q colored_man_underline; and set -l underline (set_color -u $colored_man_underline); or set -l underline (set_color -u 5BA502) # afafd7
 
     set -l end (set_color normal)
 
